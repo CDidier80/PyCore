863e581d32346afe2213613dcf727bc33408bec6
Collin Didier <collin.didier80@gmail.com>
2020-10-20 12:11:44 -0500

    landing page finished

---
+++ javascript/landingPage.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+
+
+const subtitle = document.querySelector('#subtitle')
+const beginButton = document.querySelector('#begin')
+const threeCores = document.querySelector('threeCores')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
