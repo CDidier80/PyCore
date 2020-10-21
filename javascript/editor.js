50f7fbdf536696e79578c25818003dcf6ba922f6
Collin Didier <collin.didier80@gmail.com>
2020-10-21 18:52:23 -0500

    [editor.js] redo submitButton event listener

--- javascript/editor.js
+++ javascript/editor.js
@@ -30,9 +30,11 @@ editorMethods.init()
 submitButton.addEventListener('click', () => {
     let code = codeEditor.getValue()
     // user this or original dom selection ('editor')? --
-    let output = executeCode(code)
-    terminal.innerText = output
-    terminal.value = output + "value"
+    // let output = executeCode(code).then()
+    executeCode(code).then(function(result){terminal.value=result})
+    // terminal.innerText = output
+    // console.log("output variable value in event listener: ", output)
+    
 
 }) 
 
