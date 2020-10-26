636c67656f3a057d16c3919c7bb00120870c13ee
Collin Didier <collin.didier80@gmail.com>
2020-10-25 20:53:01 -0500

    [loading.js] add loading.js, add loading div functionality

---
+++ javascript/loading.js
@@ -0,0 +1,60 @@
+console.log("loading.js is active")
+console.log(getComputedStyle(document.querySelector('.pageContent')))
+
+
+
+
+// document.onreadystatechange = function() { page
+//     if (document.readyState !== "complete") { 
+//         body.style.visibility = "hidden"
+//         loader.style.visibility = "visible"
+//     } else { 
+//         loader.style.display = "none"
+//         body.style.visibility = "visible"; 
+//     } 
+// }
+
+
+// setTimeout(function(){
+//     // console.log('loaded')
+//     // const loader = document.getElementById("loader")
+//     // console.log(loader)
+//     // loader.style.visibility = 'hidden'
+//     const pageContent = document.querySelector(".pageContent")
+//     // console.log(pageContent)
+//     // console.log(loader.style.visibility)
+
+//     // if(loader.style.visibility === 'visible') {
+//     //     console.log('if statement triggered')
+//     //     loader.visibility = 'hidden'
+//     //     pageContent.visibility = 'visible'
+//     // }
+//     // if(loader.style.visibility === 'hidden'){
+//     //     console.log('hidden')
+//     // }
+
+//     pageContent.style.visibility = 'hidden'
+
+
+
+    
+// }, 5000)
+
+
+window.onload = (event) => {
+    console.log('loaded')
+
+    const loader = document.getElementById("loader")
+    const loaderStyle = getComputedStyle(loader)
+
+    const pageContent = document.querySelector(".pageContent")
+    let pageContentStyle = getComputedStyle(pageContent)
+
+
+    if(loaderStyle.getPropertyValue('visibility') === 'visible') {
+        loader.style.visibility = 'hidden'
+        pageContent.style.visibility = 'visible'
+    }
+
+  }
+
