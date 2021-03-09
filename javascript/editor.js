a74e1e92e83da3e76714f783831def722dc1ece6
Collin Didier <collin.didier80@gmail.com>
2021-03-09 00:10:28 -0600

    add ace terminal element

--- javascript/editor.js
+++ javascript/editor.js
@@ -1,13 +1,13 @@
 // requests routed through Collin's proxy server
-
 const executeCode = async(userCode) => {
+    const endpoint = "https://collin-didier-proxy.herokuapp.com/pycore/execute"
     try {
         let userCodePayload = {
             script : userCode,
             versionIndex : "3",
             language : "python3",
         }
-        const proxyResponse = await axios.post("https://collin-didier-proxy.herokuapp.com/pycore/execute", userCodePayload)
+        const proxyResponse = await axios.post(endpoint, userCodePayload)
         const { output: executedCode } = proxyResponse.data
         return executedCode
     } catch(error) {
@@ -37,14 +37,35 @@ function changeErrorMessage(shellResponse) {
 
 
 const aceEditor = document.querySelector('#ace-editor')
+let greeting = `# write & execute Python code
+greeting = "Welcome to PyCore"
+print(greeting)
+`
 
+// let greeting = "hello"
 const editor = ace.edit(aceEditor)
 
 let editorMethods = {
     init(){
         editor.setTheme('ace/theme/tomorrow_night_blue')
         editor.session.setMode('ace/mode/python')
+        editor.setValue(greeting)
+        editor.clearSelection()
     }
 }
 
 editorMethods.init()
+
+
+
+const aceTerminal = document.querySelector("#terminal")
+const terminal = ace.edit(aceTerminal)
+
+let terminalMethods = {
+    init(){
+        editor.setTheme()
+        editor.session.setMode('ace/mode/python')
+        editor.setValue(">")
+        
+    }
+}
